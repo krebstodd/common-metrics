0268e6ff41f9b94d68f0cd975ca62ee93979ee22
Craig Offutt <craig@blispay.com>
2015-08-18 08:26:07 -0400

    Removed print statement.

--- src/main/java/com/blispay/common/metrics/reporter/BpJmxReporter.java
+++ src/main/java/com/blispay/common/metrics/reporter/BpJmxReporter.java
@@ -38,8 +38,6 @@ public class BpJmxReporter implements BpMetricConsumer {
 
     @Override
     public void registerMetric(final BpMetric metric) {
-
-        System.out.println(metric.getName());
         metrics.put(metric.getName(), metric);
     }
 
