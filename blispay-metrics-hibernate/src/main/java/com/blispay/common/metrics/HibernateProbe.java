f3ceac8453298e1c33792ffc90d68b64c3b57670
Craig Offutt <craig@blispay.com>
2015-08-28 12:14:18 -0400

    Added hibernate metrics for individual hql queries.

--- blispay-metrics-hibernate/src/main/java/com/blispay/common/metrics/HibernateProbe.java
+++ blispay-metrics-hibernate/src/main/java/com/blispay/common/metrics/HibernateProbe.java
@@ -1,9 +1,17 @@
 package com.blispay.common.metrics;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.internal.SessionFactoryImpl;
 import org.hibernate.jpa.HibernateEntityManagerFactory;
+import org.hibernate.service.spi.ServiceBinding;
+import org.hibernate.service.spi.ServiceRegistryImplementor;
+import org.hibernate.stat.QueryStatistics;
+import org.hibernate.stat.internal.ConcurrentStatisticsImpl;
+import org.hibernate.stat.spi.StatisticsImplementor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.ConcurrentHashMap;
 
 public class HibernateProbe extends BpMetricProbe {
 
@@ -13,16 +21,28 @@ public class HibernateProbe extends BpMetricProbe {
 
     private final String id;
 
+    private final Boolean collectQueryStats;
+
+    private final ConcurrentHashMap<String, BpGauge> queryGauges = new ConcurrentHashMap<>();
+
     /**
      * Create a new hibernate probe for the provided entity manager factory.
      *
      * @param entityManager Entity manager factory containing the hibernate session factory we want to probe.
-     * @param id An identifier for the specific entity manager factory as apps may have multiple entity managers.
+     * @param id An identifier for the specific entity manager factory as apps may have multiple entity managers they want to probe.
+     * @param collectQueryStats Collect statistics on a per-query basis.
      */
-    public HibernateProbe(final HibernateEntityManagerFactory entityManager, final String id) {
+    public HibernateProbe(final HibernateEntityManagerFactory entityManager, final String id, final Boolean collectQueryStats) {
+
         this.sessionFactory = entityManager.getSessionFactory();
         this.sessionFactory.getStatistics().setStatisticsEnabled(true);
         this.id = id;
+        this.collectQueryStats = collectQueryStats;
+
+        if (this.collectQueryStats) {
+            replaceStatisticsServiceBinding();
+        }
+
         init();
     }
 
@@ -35,6 +55,22 @@ public class HibernateProbe extends BpMetricProbe {
         return id + "." + metric;
     }
 
+    private void replaceStatisticsServiceBinding() {
+        final SessionFactoryImpl impl = (SessionFactoryImpl) sessionFactory;
+        final ServiceRegistryImplementor reg = impl.getServiceRegistry();
+        final ServiceBinding<StatisticsImplementor> binding = reg.locateServiceBinding(StatisticsImplementor.class);
+        binding.setService(new NewQueryStatisticsInterceptor());
+    }
+
+    private BpGauge addQueryGauge(final String query) {
+        return metricService.createGauge(HibernateProbe.class, query, "Hibernate statistics related to a masked query string.",
+                () -> this.getQueryStats(query));
+    }
+
+    private QueryStatistics getQueryStats(final String query) {
+        return sessionFactory.getStatistics().getQueryStatistics(query);
+    }
+
     @Override
     protected void startProbe() {}
 
@@ -42,4 +78,16 @@ public class HibernateProbe extends BpMetricProbe {
     protected Logger getLogger() {
         return LOG;
     }
+
+    private class NewQueryStatisticsInterceptor extends ConcurrentStatisticsImpl {
+
+        @Override
+        public QueryStatistics getQueryStatistics(final String hqlString) {
+            System.out.println("ADDING NEW QUERY: " + queryGauges.containsKey(hqlString));
+            queryGauges.computeIfAbsent(hqlString, HibernateProbe.this::addQueryGauge);
+            return super.getQueryStatistics(hqlString);
+        }
+
+    }
+
 }
