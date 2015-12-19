2765753e38db73c2d53fd1ec81a634664d153fb1
Dustin Conrad <dustin@blispay.com>
2015-12-19 00:57:02 -0500

    fix checkstyle

--- blispay-metrics-jetty/src/test/java/com/blispay/common/metrics/JettyProbeTest.java
+++ blispay-metrics-jetty/src/test/java/com/blispay/common/metrics/JettyProbeTest.java
@@ -22,7 +22,6 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
